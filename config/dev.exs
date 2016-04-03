3d706b5896e6e0e01442f66b7ea5798a635c23ad
Toby Privett <toby@snaplab.co.uk>
2016-04-03 15:41:36 +0100

    Updating the database config

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "toby",
+  password: "",
   database: "rumbl_dev",
   hostname: "localhost",
   pool_size: 10
