f48bfd2344e47d1293e7b26e3c30a0863be78cc9
Toby Privett <toby@snaplab.co.uk>
2016-04-11 10:48:53 +0100

    Validate and encrypt the user's password

--- web/models/user.ex
+++ web/models/user.ex
@@ -15,4 +15,26 @@ defmodule Rumbl.User do
     |> cast(params, ~w(name username), [])
     |> validate_length(:username, min: 1, max: 20)
   end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 20)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+
+      # this is behaviour if the changeset is valid
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+
+      # when there is no match on false, just return the untouched changeset
+      # (took me a while to figure out what the underscore was doing!)
+      _ ->
+        changeset
+    end
+  end
 end
