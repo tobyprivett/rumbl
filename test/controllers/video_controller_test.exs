b28bafca7c32249ecc6647a55e501be0ddb135c3
Toby Privett <toby@snaplab.co.uk>
2016-04-13 12:38:57 +0100

    Test the logged in user
    Note the use of @tag to let us toggle logged in state

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,6 +1,27 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
 
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: "max")
+      conn = assign(conn(), :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
+
+  @tag login_as: "max"
+  test "List all user's videos on index", %{conn: conn, user: user} do
+    user_video = insert_video(user, title: "stupid cats")
+    other_video = insert_video(user, title: "another video")
+
+    conn = get conn, video_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Listing videos/
+    assert String.contains?(conn.resp_body, user_video.title)
+    assert String.contains?(conn.resp_body, other_video.title)
+  end
+
   test "requires auth on all actions", %{conn: conn} do
     Enum.each([
       get(conn, video_path(conn, :new)),
